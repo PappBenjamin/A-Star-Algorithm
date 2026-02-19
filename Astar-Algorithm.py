ec52e1c4e0b38396b8adb0b9ac7b619ed71e2c86
Papp Benjámin <pappbenjamin@yahoo.com>
2026-02-19 14:20:18 +0200

    Modified algorithm

--- Astar-Algorithm.py
+++ Astar-Algorithm.py
@@ -4,339 +4,238 @@ from time import sleep
 
 
 def center_gui(root):
-
-    # Gets the requested values of the height and widht.
-    windowWidth = root.winfo_reqwidth()
-    windowHeight = root.winfo_reqheight()
-
-    # Gets both half the screen width/height and window width/height
-    positionRight = int(root.winfo_screenwidth()/2 - windowWidth/2)
-    positionDown = int(root.winfo_screenheight()/2 - windowHeight/2)
-
-    # Positions the window in the center of the page.
-    root.geometry("+{}+{}".format(positionRight, positionDown))
-
-
-def pop_up_window(app):
-    """
-    Displays the start up window with the instructions.
-    When the pop up is displayed the buttons at the backround
-    are disabled
-    """
-
-    def start_button_action():
-
-        app.enable_buttons()
-        win.destroy()
-
-    win = Toplevel()
-    win.wm_title("Welcome")
-
-    Label(win, text="Step 1: Select starting point",
-          font=("Calibri", 13), pady=5, padx=10).pack()
-    Label(win, text="Step 2: Select end point", font=(
-        "Calibri", 13), pady=5, padx=10).pack()
-    Label(win, text="Step 3: Select Obstacles", font=(
-        "Calibri", 13), pady=5, padx=10).pack()
-    Label(win, text="Click and hover.Then click again to stop", padx=25).pack()
-    Label(win, text="Step 4: Press Enter to start",
-          font=("Calibri", 13), pady=5, padx=10).pack()
-    Label(win, text="Step 5: Press R to restart",
-          font=("Calibri", 13), pady=5, padx=10).pack()
-    Button(win, text="Start", command=start_button_action,
-           ).pack()
-
-    win.update_idletasks()
-    center_gui(win)
+    windowWidth = 700
+    windowHeight = 750
+    positionRight = int(root.winfo_screenwidth() / 2 - windowWidth / 2)
+    positionDown = int(root.winfo_screenheight() / 2 - windowHeight / 2)
+    root.geometry(f"{windowWidth}x{windowHeight}+{positionRight}+{positionDown}")
 
 
 class App:
+    GRID_SIZE = 9
+    CELL_SIZE = 140
 
     def __init__(self, master):
-
         self.master = master
         master.wm_title("A* Algorithm")
-        self.buttons = []
-        self.start = []
-        self.goal = []
-        self.obstacles = []
+        master.resizable(False, False)
+
+        self.cells = []
+        self.start = None
+        self.goal = None
+        self.obstacles = set()
         self.mode = 0
+        self.is_drawing = False
 
-        for i in range(25):
-            self.buttons.append([])
-            for j in range(25):
+        # Create grid of Labels with larger size
+        for i in range(self.GRID_SIZE):
+            self.cells.append([])
+            for j in range(self.GRID_SIZE):
+                cell = Label(master, width=self.CELL_SIZE // 10, height=self.CELL_SIZE // 15,
+                             font=('Arial', 7, 'bold'),
+                             bg="white", relief="raised", bd=2)
+                cell.bind('<Button-1>', partial(self.on_click, i, j))
+                cell.bind('<B1-Motion>', partial(self.on_drag, i, j))
+                cell.bind('<ButtonRelease-1>', self.stop_drawing)
+                self.cells[i].append(cell)
+                self.cells[i][j].grid(row=i, column=j)
 
-                # Initiliaze buttons
-                button = Button(master, width=2, height=1,
-                                command=partial(self.button_operation, i, j), state="disabled")
-
-                self.buttons[i].append(button)
-
-                # This event is used for the obstacle setting
-                self.buttons[i][j].bind('<Enter>', partial(
-                    self.add_obstacle, i, j))
-
-                self.buttons[i][j].grid(row=i, column=j)
-
-        master.update_idletasks()
         center_gui(master)
 
-        pop_up_window(self)
-
-    def enable_buttons(self):
-
-        for i in range(25):
-            for j in range(25):
-                self.buttons[i][j].configure(state="normal")
-
-    def disable_buttons(self):
-
-        for i in range(25):
-            for j in range(25):
-                self.buttons[i][j].configure(state="disable")
-
-    # Every time a button is clicked this function is triggered
-    # This function is responsible for controling the flow of the program
-
-    def button_operation(self, row, column):
-        """
-        According to the value of 'mode' this fuction
-        sets the value of start and end. Also by changing
-        the value of mode it controls when we can set obstacles and
-        when we can start the algorithm
-        """
-
-        # Set start mode
+    def on_click(self, row, column, event):
         if self.mode == 0:
-
-            self.start.append(row)
-            self.start.append(column)
+            self.start = (row, column)
             self.mode = 1
-            self.buttons[row][column].configure(bg='green')
-
-        # Set end mode
+            self.cells[row][column].configure(bg='#4CAF50')
+            print(f"Start set at ({row}, {column})")
         elif self.mode == 1:
-
-            self.goal.append(row)
-            self.goal.append(column)
+            self.goal = (row, column)
             self.mode = 2
-            self.buttons[row][column].configure(bg='red')
-
+            self.cells[row][column].configure(bg='#F44336')
+            print(f"Goal set at ({row}, {column})")
         elif self.mode == 2:
-
-            # Set to set obstacles mode => By hovering over buttons
             self.mode = 3
-
-        else:
-            # When the mode = 2 the user cant set obstacles by hovering and the algorithm can start
-            self.mode = 2
-
-    def add_obstacle(self, row, column, event):
-
-        # Checks if we are in the obstacle setting mode
+            print("Obstacle mode activated - drag to draw obstacles")
+        # Once in mode 3 (obstacle mode), clicking does nothing - only dragging adds obstacles
+        self.is_drawing = True
         if self.mode == 3:
-            obstacle_node = []
-            obstacle_node.append(row)
-            obstacle_node.append(column)
+            self.on_drag(row, column, event)
 
-            self.obstacles.append(obstacle_node[:])
-            self.buttons[row][column].configure(bg='black')
+    def on_drag(self, row, column, event):
 
+        """
+        This method is called when the user drags the mouse over cells.
+        """
+        if self.mode == 3 and self.is_drawing:
+            # Don't allow obstacles at start or goal positions
+            if (row, column) != self.start and (row, column) != self.goal:
+                # Add obstacle if it doesn't already exist
+                if (row, column) not in self.obstacles:
+                    self.obstacles.add((row, column))
+                    # Color it dark gray to show it's an obstacle
+                    self.cells[row][column].configure(bg='#424242')
+
+    def stop_drawing(self, event):
+        self.is_drawing = False
+
+    # calcualte Manhattan distance as heuristic
     def heuristic(self, node1, node2):
-        result = abs(node1[0] - node2[0]) + abs(node1[1]-node2[1])
-        return result
+        return abs(node1[0] - node2[0]) + abs(node1[1] - node2[1])
 
-    def find_neighbors(self, current, obstacles):
-
+    def find_neighbors(self, current):
         neighbors = []
+        row, col = current
 
-        # With current[:] I create a new list and I dont use the pointer to the original list otherwise the end result whould have same lists
+        # Cardinal directions only (no diagonal through walls)
+        # Up, Down, Right, Left
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-        right_neighbor = current[:]
-        right_neighbor[1] = current[1] + 1
-
-        if 0 <= right_neighbor[1] < 25 and right_neighbor not in self.obstacles:
-            neighbors.append(right_neighbor)
-
-        left_neighbor = current[:]
-        left_neighbor[1] = current[1] - 1
-
-        if 0 <= left_neighbor[1] < 25 and left_neighbor not in self.obstacles:
-            neighbors.append(left_neighbor)
-
-        up_neighbor = current[:]
-        up_neighbor[0] = current[0] + 1
-
-        if 0 <= up_neighbor[0] < 25 and up_neighbor not in self.obstacles:
-
-            neighbors.append(up_neighbor)
-
-        down_neighbor = current[:]
-        down_neighbor[0] = current[0] - 1
-
-        if 0 <= down_neighbor[0] < 25 and down_neighbor not in self.obstacles:
-
-            neighbors.append(down_neighbor)
-
-        down_right_neighbor = current[:]
-        down_right_neighbor[0] = current[0] + 1
-        down_right_neighbor[1] = current[1] + 1
-
-        if 0 <= down_right_neighbor[0] < 25 and 0 <= down_right_neighbor[1] < 25 and down_right_neighbor not in self.obstacles:
-            neighbors.append(down_right_neighbor)
-
-        up_right_neighbor = current[:]
-        up_right_neighbor[0] = current[0] - 1
-        up_right_neighbor[1] = current[1] + 1
-
-        if 0 <= up_right_neighbor[0] < 25 and 0 <= up_right_neighbor[1] < 25 and up_right_neighbor not in self.obstacles:
-
-            neighbors.append(up_right_neighbor)
-
-        up_left_neighbor = current[:]
-        up_left_neighbor[0] = current[0] - 1
-        up_left_neighbor[1] = current[1] - 1
-
-        if 0 <= up_left_neighbor[0] < 25 and 0 <= up_left_neighbor[1] < 25 and up_left_neighbor not in self.obstacles:
-
-            neighbors.append(up_left_neighbor)
-
-        down_left_neighbor = current[:]
-        down_left_neighbor[0] = current[0] + 1
-        down_left_neighbor[1] = current[1] - 1
-
-        if 0 <= down_left_neighbor[0] < 25 and 0 <= down_left_neighbor[1] < 25 and down_left_neighbor not in self.obstacles:
-            neighbors.append(down_left_neighbor)
-
+        for dr, dc in directions:
+            new_row, new_col = row + dr, col + dc
+            if 0 <= new_row < self.GRID_SIZE and 0 <= new_col < self.GRID_SIZE:
+                neighbor = (new_row, new_col)
+                if neighbor not in self.obstacles:
+                    neighbors.append(neighbor)
         return neighbors
 
-    def sort_open_set(self, open_set, f_score):
-
-        # The index of the list is the same as the index in the open set
-        # and the value of the index is the f_score of it
-        index_to_fscore = []
-
-        for node in open_set:
-            f_score_of_node = f_score[node[0]][node[1]]
-            index_to_fscore.append(f_score_of_node)
-
-        sorted_copy = index_to_fscore.copy()
-        sorted_copy.sort()
-
-        sorted_open_set = []
-
-        for value in sorted_copy:
-            min = index_to_fscore.index(value)
-            sorted_open_set.append(open_set[min])
-            # We mark that we have transfered this value to the sorted array
-            index_to_fscore[min] = float('inf')
-
-        return sorted_open_set
-
     def reconstruct_path(self, cameFrom, current):
-        total_path = []
 
-        while current != self.start:
+        """
+        This method traces back the shortest path from the goal to the start.
+        It uses the cameFrom dictionary which was built during A* algorithm execution.
+        """
 
-            self.buttons[current[0]][current[1]].configure(bg='red')
-
-            total_path.append(current[:])
-            current = cameFrom[current[0]][current[1]]
+        total_path = [current]
+        # Keep following the cameFrom chain until we reach the start
+        while current in cameFrom and cameFrom[current] is not None:
+            current = cameFrom[current]
+            total_path.append(current)
+        # Reverse to get path from start to goal (instead of goal to start)
+        total_path.reverse()
+        return total_path
 
     def a_star_algorithm(self, start, goal):
+        """
+        A* (A-Star) pathfinding algorithm - finds the shortest path from start to goal.
 
+        Key concepts:
+        - open_set: Nodes we can explore next
+        - g_score: Actual distance from start to each node
+        - f_score: g_score + heuristic (estimated total distance)
+        - came_from: Tracks which node each cell came from (for path reconstruction)
+
+        The algorithm picks the node with lowest f_score from open_set and explores its neighbors.
+        If a better path to a neighbor is found, we update it and add to open_set.
+        """
+        if start is None or goal is None:
+            print("Start or goal not set!")
+            return
+
+        # Initialize: nodes we can explore next
         open_set = [start]
-        g_score = []
-        f_score = []
-        came_from = []
 
-        # Initialiazation of g_score and came_from
-        for i in range(25):
-            f_score.append([])
-            g_score.append([])
-            came_from.append([])
-            for j in range(25):
-                temp = float('inf')
-                came_from[i].append([])
-                g_score[i].append(temp)  # set it to infinity
-                f_score[i].append(temp)  # set it to infinity
+        # Start with all infinity, then set start to 0
+        g_score = [[float('inf')] * self.GRID_SIZE for _ in range(self.GRID_SIZE)]
 
+        # f_score[row][col] = g_score + heuristic (estimated total cost)
+        # Used to decide which node to explore next
+        f_score = [[float('inf')] * self.GRID_SIZE for _ in range(self.GRID_SIZE)]
+
+        # Dictionary to store: came_from[node] = previous_node (for path reconstruction)
+        came_from = {}
+
+        # Initialize start node
         g_score[start[0]][start[1]] = 0
         f_score[start[0]][start[1]] = self.heuristic(start, goal)
 
-        while len(open_set) > 0:
-            self.master.update_idletasks()
-            sleep(0.02)
+        # Main loop: continue while there are nodes to explore
+        while open_set:
+            self.master.update()  # Update GUI to show animation
+            sleep(0.02)  # Slow down animation for visibility
 
-            open_set = self.sort_open_set(open_set, f_score)
-
+            # Sort open_set by f_score and pick the one with lowest cost
+            open_set = sorted(open_set, key=lambda node: f_score[node[0]][node[1]])
             current = open_set[0]
-            current_row = current[0]
-            current_column = current[1]
 
+            # If we reached the goal, reconstruct and visualize the path
             if current == goal:
-                return self.reconstruct_path(came_from, current)
+                path = self.reconstruct_path(came_from, current)
+                self.visualize_path(path)
+                return
 
+            # Remove current node from open_set (we're processing it now)
             open_set.remove(current)
 
-            neighbors = self.find_neighbors(current, [])
+            # Get all neighboring cells (up, down, left, right)
+            neighbors = self.find_neighbors(current)
 
-            for node in neighbors:
+            # Check each neighbor
+            for neighbor in neighbors:
+                # Calculate distance: current distance + 1 (cost to neighbor)
+                tentative_gScore = g_score[current[0]][current[1]] + 1
 
-                node_row = node[0]
-                node_column = node[1]
+                # If we found a better path to this neighbor
+                if tentative_gScore < g_score[neighbor[0]][neighbor[1]]:
+                    # Record that we came from 'current' to reach 'neighbor'
+                    came_from[neighbor] = current
 
-                # The weight of every edge is 1
-                tentative_gScore = g_score[current_row][current_column] + 1
+                    # Update the actual distance to this neighbor
+                    g_score[neighbor[0]][neighbor[1]] = tentative_gScore
 
-                if tentative_gScore < g_score[node_row][node_column]:
+                    # Update the estimated total distance (g + heuristic)
+                    f_score[neighbor[0]][neighbor[1]] = g_score[neighbor[0]][neighbor[1]] + self.heuristic(neighbor,
+                                                                                                           goal)
 
-                    came_from[node_row][node_column].append(current_row)
-                    came_from[node_row][node_column].append(
-                        current_column)
+                    # If this neighbor isn't in open_set yet, add it for future exploration
+                    if neighbor not in open_set:
+                        # Color it light blue to show it's been explored
+                        if neighbor != start and neighbor != goal:
+                            self.cells[neighbor[0]][neighbor[1]].configure(bg='#64B5F6')
+                        open_set.append(neighbor)
 
-                    g_score[node_row][node_column] = tentative_gScore
+        # If we exit the loop and haven't found the goal, no path exists
+        print("No path found!")
 
-                    f_score[node_row][node_column] = g_score[node_row][node_column] + \
-                        self.heuristic(node, self.goal)
-
-                    if node not in open_set:
-
-                        self.buttons[node[0]][node[1]].configure(bg='blue')
-                        open_set.append(node[:])
-
-        print("fail!")
+    def visualize_path(self, path):
+        for i, node in enumerate(path):
+            row, col = node
+            if node == self.start:
+                self.cells[row][col].configure(bg='#4CAF50')
+            elif node == self.goal:
+                self.cells[row][col].configure(bg='#F44336')
+            else:
+                self.cells[row][col].configure(bg='#FFEB3B')
+            if i < len(path) - 1:
+                self.master.update()
+                sleep(0.05)
 
     def find_path(self, event):
-
-        # Checks if we are in the correct mode to start the algorithm
-        if self.mode == 2:
+        # Allow pathfinding from mode 2 or mode 3
+        if self.mode == 2 or self.mode == 3:
+            print("Starting A* algorithm...")
             self.a_star_algorithm(self.start, self.goal)
-            self.disable_buttons()
+            self.mode = 4
+        else:
+            print("Please set start point, goal point, and draw obstacles first")
 
     def reset(self, event):
+        self.start = None
+        self.goal = None
+        self.obstacles = set()
+        self.mode = 0
 
-        if self.mode == 2:
-            self.start = []
-            self.goal = []
-            self.obstacles = []
-            self.mode = 0
+        for i in range(self.GRID_SIZE):
+            for j in range(self.GRID_SIZE):
+                self.cells[i][j].configure(bg='white')
 
-            for i in range(25):
-                for j in range(25):
-
-                    self.buttons[i][j].configure(bg='SystemButtonFace')
-
-            self.enable_buttons()
+        print("Reset complete")
 
 
 if __name__ == '__main__':
     root = Tk()
     app = App(root)
 
-    # Starts the algorithm when we press enter
     root.bind('<Return>', app.find_path)
-    # Resets when we press 'R'
     root.bind('r', app.reset)
 
     root.mainloop()
